0374c093dce073628e33b1294b0411ceeacadd8f
MarvelSQ <sun1993hlj@hotmail.com>
2022-08-20 23:58:03 +0800

    fix: fix packages commonjs compile

--- apps/site/src/index.tsx
+++ apps/site/src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import { main } from "core";
+import * as core from "@marvelsq/i18n-core";
 
 const instance = createRoot(document.getElementById("root")!);
 
 instance.render(<div>test</div>);
 
-console.log(main.compute());
+console.log(core.main.compute());
--- apps/site/vite.config.js
+++ apps/site/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: ["@marvelsq/i18n-core"],
+  },
   esbuild: {
     jsx: "automatic",
+  },
+  build: {
+    commonjsOptions: {
+      include: [/i18n\/packages/, /node_modules/],
+    },
   },
 });
